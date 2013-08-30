942f0bd41681643c98d2e50451597d7a854a7763
Mark Essel <messel@gmail.com>
2013-08-30 09:54:42 -0400

    duh no main loop blocking sleep, added one

--- test/test.js
+++ test/test.js
@@ -7,5 +7,18 @@ console.log('activating 3 death clocks');
 var aDeathClock = new DeathClock(1,'end of the world a','',10000);
 var bDeathClock = new DeathClock(2,'end of the world b','',10000);
 var cDeathClock = new DeathClock(3,'end of the world c','',10000);
+
+aDeathClock.Stop();
+bDeathClock.Stop();
+
+function sleep(millis)
+{
+ 	var date = new Date();
+  	var curDate = null;
+  	do { curDate = new Date(); }
+  	while(curDate-date < millis);
+}
+
 sleep(11000);
-console.log( 'should never get here' ); // end of world a triggers at 1 second
+
+console.log( 'should never get here' ); // end of world c triggers at 3 seconds
